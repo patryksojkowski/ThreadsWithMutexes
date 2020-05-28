138d252f87218bb265b9fee258e3f1bb62e16828
Patryk Sojkowski <patryk.sojkowski@ericsson.com>
2020-05-28 15:23:19 +0200

    Add project files.

---
+++ ThreadsWithMutex/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsWithMutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IncThread myt1 = new IncThread();
+            DecThread myt2 = new DecThread();
+            Console.Read();
+        }
+
+        class MyCounter
+        {
+            public static int count = 0;
+            public static Mutex MuTexLock = new Mutex();
+        }
+        class IncThread
+        {
+            public Thread th;
+            public IncThread()
+            {
+                th = new Thread(this.Go);
+                th.Start();
+            }
+            void Go()
+            {
+                Console.WriteLine("IncThread is waiting for the mutex.");
+                MyCounter.MuTexLock.WaitOne();
+                Console.WriteLine("IncThread acquires the mutex.");
+                int num = 10;
+                do
+                {
+                    Thread.Sleep(50);
+                    MyCounter.count++;
+                    Console.WriteLine("In IncThread, MyCounter.count is " + MyCounter.count);
+                    num--;
+                } while (num > 0);
+                Console.WriteLine("IncThread releases the mutex.");
+                MyCounter.MuTexLock.ReleaseMutex();
+            }
+        }
+        class DecThread
+        {
+            public Thread th;
+            public DecThread()
+            {
+                th = new Thread(this.Go);
+                th.Start();
+            }
+            void Go()
+            {
+                Console.WriteLine("DecThread is waiting for the mutex.");
+                MyCounter.MuTexLock.WaitOne();
+                Console.WriteLine("DecThread acquires the mutex.");
+                int num = 10;
+                do
+                {
+                    Thread.Sleep(50);
+                    MyCounter.count--;
+                    Console.WriteLine("In DecThread, MyCounter.count is " + MyCounter.count);
+                    num--;
+                } while (num > 0);
+                Console.WriteLine("DecThread releases the mutex.");
+                MyCounter.MuTexLock.ReleaseMutex();
+            }
+        }
+    }
+}
---
+++ ThreadsWithWaitOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsWithWaitOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(CountUp))
+            {
+                Name = "||||||||||||",
+            };
+            Thread t2 = new Thread(new ThreadStart(CountUp))
+            {
+                Name = "------------",
+            };
+
+            t1.Start();
+            t2.Start();
+            Console.Read();
+        }
+
+        private static int Runs = 0;
+        static Mutex mutex = new Mutex(false, "RunsMutex");
+        public static void CountUp()
+        {
+            while (Runs < 100)
+            {
+                // acquire the mutex  
+                mutex.WaitOne();
+                Runs++;
+                Console.WriteLine(Thread.CurrentThread.Name + " " + Runs);
+                Thread.Sleep(200);
+                // release the mutex  
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
